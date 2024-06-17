b84b64e45455410f00941d4f400aa11e6dabac0a
Tushar H <tusharjain2201@gmail.com>
2024-06-17 21:59:06 +0530

    Initial setup for the tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Start from './pages/start/Start'
+import Game from './pages/game/Game'
+
+function App() {
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/game' element={<Game />} />
+        <Route path='/over' element={<Over />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
