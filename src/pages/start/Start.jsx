2464f4d68c76e9b9bf490955b9500cd2152c93fb
Tushar H <tusharjain2201@gmail.com>
2024-06-27 14:15:18 +0530

    Add effects to start page

--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -1,22 +1,29 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './Start.css';
-import { notch, x, o } from '../../assets/assets';
+import { notch, x, o, board1, board2 } from '../../assets/assets';
 import { GameContext } from '../../context/GameContext';
+import woodHit from '../../assets/audio/woodHit.mp3';
 
 const Start = () => {
   const { turnNext } = useContext(GameContext);
   const navigate = useNavigate();
+  const woodHitSound = new Audio(woodHit);
 
   const startGame = (choice) => {
     turnNext(choice);
+    woodHitSound.play();
     navigate('/game');
   }
 
   return (
     <div className='mn-start'>
-        <div className='mn-start__board1'></div>
-        <div className='mn-start__board2'></div>
+        <div className='mn-start__board1'>
+            <img src={board1} alt='board1' />
+        </div>
+        <div className='mn-start__board2'>
+            <img src={board2} alt='board2'/>
+        </div>
         <div className='mn-start__text'>
             <div className='mn-start__title'>tic tac toe</div>
             <div className='mn-start__desc'>2 players</div>
@@ -28,10 +35,10 @@ const Start = () => {
                     <img src={notch} alt='notch' />
                 </div>
                 <div className='mn-button__select'>
-                    <div className='mn-select__opt' onClick={() => startGame('x')}>
+                    <div className='mn-select__opt x' onClick={() => startGame('x')}>
                         <img src={x} alt='x' />
                     </div>
-                    <div className='mn-select__opt' onClick={() => startGame('o')}>
+                    <div className='mn-select__opt y' onClick={() => startGame('o')}>
                         <img src={o} alt='o' />
                     </div>
                 </div>
